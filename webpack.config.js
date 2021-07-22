c39034abec427881197f1239c380050f97f478ae
Alexander Ponomarenko <madi-al@yandex.ru>
2021-07-22 22:50:27 +0300

    done: shop-page (adaptiv, Webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
-  entry: { main: './js/index.js' },
-  output: {
+    entry: { main: './js/index.js' },
+    output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
         publicPath: ''
   },
-  mode: 'development',
+    mode: 'development',
   devServer: {
-    contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-
-    open: true // сайт будет открываться сам при запуске npm run dev
+    contentBase: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
+  },
+    module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+        },
+          // Добавьте postcss-loader
+        'postcss-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
     }),
     new CleanWebpackPlugin(), // использовали плагин
-  ]
-} 
+    new MiniCssExtractPlugin()
+  ] 
+};
