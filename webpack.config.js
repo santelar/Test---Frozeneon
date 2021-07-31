11cf8323b815dde63aa61b9e68f4f8d5f2d1b2f2
Alexander Ponomarenko <madi-al@yandex.ru>
2021-07-31 16:45:43 +0300

    refactor: images path

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './index.html'
     }),
     new CleanWebpackPlugin(), // использовали плагин
     new MiniCssExtractPlugin(),
